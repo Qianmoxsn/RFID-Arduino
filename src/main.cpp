63509298adf06bfd282c55774ee5b33de7fe6001
Qianmo <92790790+Qianmoxsn@users.noreply.github.com>
2024-04-16 15:41:05 +0800

    lab1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,70 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+unsigned long timerecord = 0;
+unsigned long time_1s = 1000;
+unsigned long time_2s = 2000;
+bool led_state = 1;
+
+void task1setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void task1loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(2000);
+}
+
+
+void task2setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void task2loop() {
+  if (millis() - timerecord > time_1s && led_state == 0) {
+    timerecord = millis();
+    digitalWrite(LED_BUILTIN, led_state);
+    led_state = !led_state;
+  }
+  if (millis() - timerecord > time_2s && led_state == 1) {
+    timerecord = millis();
+    digitalWrite(LED_BUILTIN, led_state);
+    led_state = !led_state;
+  }
+}
+
+
+void task3setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(7, INPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.begin(9600);
+}
+
+void task3loop() {
+  int button_state = digitalRead(7);
+  Serial.println(button_state);
+  if (button_state == HIGH) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  delay(300);
+}
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // task1setup();
+  // task2setup();
+  task3setup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // task1loop();
+  // task2loop();
+  task3loop();
 }
